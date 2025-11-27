c5dd6ea30243dcafc260f18dc35addd9242a719c
raghuramulu-w <raghu0711@gmail.com>
2025-11-27 14:06:46 +0530

    hai

--- minikube.tf
+++ minikube.tf
@@ -4,7 +4,7 @@ module "minikube" {
   aws_region    = "us-east-1"
   cluster_name  = "roboshop-1"
   aws_instance_type = "t3.medium"
-  ssh_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDDqQmGFyh+/VzGfj2CqTBdFzexjIndpfkE45ObHUXn2/tU9pjQEGYy4nOiUmc2mHH2YcU3CJu7JlA8S6j6bHZONCVNMM4Kr2PcwPTZiTzKhx4vpOqMJd+cpuRjDWKq7zMq7q9CFsK9nS2VrJZFGrAlkCPTQ7gvicgf4DX9xZiJqvcyMyzeEB1UIm9UAy7Arn6DwA86TdoYtIe54yjsg+hs6WVYHaLdjfL/Z+GAvcZZ4UXDMSTf6Xgw76/z7wS1wacZm3xQ4R7H/Dw3YNOIJZCqavqVjYNgi3M/zFDVgLRGQysBONew6z5LcHlIzEEEuaBOspdZtrwPJyn9j/QU/WbnLN33zeKdAHePJ5lwR7y1BQzOWOdhNGcU23pmRP1qZwshPzhaLFuZsWBuXDNjPbTZn5e28i7zI358zRgE82hYOdzwraBZPFu/irqs84Ce+sxDJY/DMbfFsJaO566aSxr3hc5Qjnv/nmQG90R0H9l1TcBDI/lLk2sQhL3zUiEGWJM= DELL@DESKTOP-GBRVKFP"
+  ssh_public_key = "/c/Users/DELL/munni.pub"
 
   aws_subnet_id = "subnet-0efb935738860f410" #replace your default subnet id
   # by default centos7 will be used
